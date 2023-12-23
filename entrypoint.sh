9abd0b71c8ef84c2511b70c9fc6884c7b03bf2b4
Esanim <17294241+Esanim@users.noreply.github.com>
2023-12-24 00:10:22 +0100

    try sth new

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -l
 
 echo "H $1"
+curl -L https://dl.dagger.io/dagger/install.sh | sh
+mix deps.get
+dagger run mix elixir_with_dagger.test
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
